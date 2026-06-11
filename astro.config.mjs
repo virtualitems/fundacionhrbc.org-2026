d70bb3f0f0df38888b1a10d89f210881b4844ab3
Alejandro CR <virtualitemsuniverse@gmail.com>
2026-06-11 15:15:13 -0500

    fix deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-	outDir: './docs',
+	outDir: './dist',
 	vite: {
 		plugins: [tailwindcss()],
 	},
